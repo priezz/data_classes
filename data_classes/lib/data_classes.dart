db2a14ee4dcc0599f0b73ca5780570dfcc0c9f14
Alexandr Priezzhev <a@priezz.net>
2019-12-24 22:22:31 +0100

    Fix exports

--- data_classes/lib/data_classes.dart
+++ data_classes/lib/data_classes.dart
@@ -1,5 +1,4 @@
 export 'package:collection/collection.dart';
-export 'package:equatable/equatable.dart';
 export 'package:json_annotation/json_annotation.dart';
 export 'package:meta/meta.dart' show immutable, required;
 
