93acbd2b9b54f3ed339e5d4a253f19bc9ad8cb80
Alexandr Priezzhev <a@priezz.net>
2021-03-16 15:09:13 +0100

    Fix the use of `childrenListener` parameter

--- data_classes/lib/data_classes.dart
+++ data_classes/lib/data_classes.dart
@@ -10,7 +10,7 @@ typedef ChangeListener = Future<void> Function(
   String path, {
   Object? next,
   Object? prev,
-  Object Function()? toJson,
+  dynamic Function()? toJson,
 });
 typedef DataClassBuilder<T> = void Function(T);
 typedef DataClassAsyncBuilder<T> = Future<void> Function(T);
